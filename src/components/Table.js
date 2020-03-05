946ad1480e519850127a4c0683493ef578c6fcf6
Jethet <henriette.hettinga@gmail.com>
2020-03-05 14:25:53 +0100

    Setup table page

---
+++ src/components/Table.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+
+export default function TableForm(props) {
+  const createTable = 
+  
+
+  return (
+    <div>
+      <h1>Create a table!</h1>
+      <ul>
+        <li>Date:</li>
+        <li>Location:</li>
+        </ul>
+        <li>
+          <h2>I will bring this to the table:</h2>
+          <ul>
+            <li>hot dish</li>
+                <ul>
+                <li>vegetarian</li>
+                <li>vegan </li>
+                <li>gluten-free</li>
+                </ul>
+            <li>cold dish</li>
+                <ul>
+                <li>vegetarian</li>
+                <li>vegan </li>
+                <li>gluten-free</li>
+                </ul>
+            <li>snack</li>
+                <ul>
+                <li>vegetarian</li>
+                <li>vegan </li>
+                <li>gluten-free</li>
+                </ul>
+            <li>desert</li>
+                <ul>
+                <li>vegetarian</li>
+                <li>vegan </li>
+                <li>gluten-free</li>
+                </ul>
+            <li>drinks non-alcohol</li>
+            <li>drinks alcohol</li>
+          </ul>
+        </li>
+    </div>
+  );
+}
